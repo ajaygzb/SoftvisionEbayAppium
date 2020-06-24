65f01f0bdbd1e8260d2d11f0a174206add10f8e2
Ajay <ajaygzb10@gmail.com>
2020-06-24 10:24:16 +0530

    Added base package

---
+++ src/test/java/Pages/ProductPage.java
@@ -0,0 +1,68 @@
+package Pages;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.Assert;
+
+import base.BasePage;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+
+public class ProductPage extends BasePage {
+
+	By brandNameLocator = By.xpath("//*[@resource-id=\\\"title_feature_div\\\"]//*[@class=\\\"android.view.View\\\"]");
+	By descriptionLocator = By.xpath("//*[@resource-id=\"title_feature_div\"]//*[@class=\"android.view.View\"]");
+	By priceLocator = By
+			.xpath("//*[@resource-id=\"atfRedesign_priceblock_priceToPay\"]//*[@class=\"android.widget.EditText\"]");
+
+	By addToCartLocator = By.xpath("//*[@text=\"Added to cart\"]");
+
+	Logger logger = LoggerFactory.getLogger(ProductPage.class);
+
+	static List<AndroidElement> descList = new ArrayList<>();
+	static List<AndroidElement> priceList = new ArrayList<>();
+
+	public ProductPage(AndroidDriver<AndroidElement> aDriver) {
+		super(aDriver);
+	}
+
+	public List<String> get_Name_Price_Description() throws InterruptedException {
+
+		logger.debug("Inside get_Name_Price_Description function ");
+
+		TimeUnit.SECONDS.sleep(5);
+
+		String brandName = getText(brandNameLocator, 0).split("[0-9]")[0];
+		String description = getText(descriptionLocator, 0).split(brandName)[1];
+		String price = getText(priceLocator, 0).split(" ")[1];
+
+		System.out.println("brandName----" + brandName + "description-----" + description + "price-----" + price);
+
+		logger.debug("brandName---" + brandName);
+		logger.debug("description---" + description);
+		logger.debug("price---" + price);
+
+		List<String> itemValues = new ArrayList<String>();
+		itemValues.add(brandName);
+		itemValues.add(description);
+		itemValues.add(price);
+
+		scrollTo();
+
+		return itemValues;
+
+	}
+
+	public boolean addToCart() {
+		scrolltoElement("Add to Cart");
+		clickUsingTextContains("Add to Cart");
+		return (getText(addToCartLocator, 0).equals("Added to cart"));
+
+	}
+
+}
---
+++ src/test/java/Pages/SearchPage.java
@@ -0,0 +1,71 @@
+package Pages;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import base.BasePage;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+
+public class SearchPage extends BasePage {
+	
+	
+	By brandNameLocator = By.id("com.amazon.mShop.android.shopping:id/item_title");
+	By priceLocator = By.xpath(
+			"//*[@resource-id=\"com.amazon.mShop.android.shopping:id/rs_results_styled_price_v2\"]//*[@class=\"android.widget.TextView\"]");
+
+	Logger logger = LoggerFactory.getLogger(SearchPage.class);
+
+	static List<AndroidElement> descList = new ArrayList<>();
+	static List<AndroidElement> priceList = new ArrayList<>();
+
+	public SearchPage(AndroidDriver<AndroidElement> aDriver) {
+		super(aDriver);
+	}
+
+	public void searchItemInSearchBox(String itemName) {
+		logger.debug("Inside searchItemInSearchBox function ");
+		clickUsingTextContains("Search");
+		sendText("Search", itemName);
+		pressKey("Enter");
+
+	}
+
+	public void selectItemFromSearchList(int index) throws InterruptedException {
+		logger.debug("Inside selectItemFromSearchList function ");
+		TimeUnit.SECONDS.sleep(7);
+		clickOnRandomItem(brandNameLocator, index);
+
+	}
+
+	public void clickOnRandomItem(By locator, int index) {
+		logger.debug("Inside clickOnRandomItem function");
+		findElementAndClick(locator, index);
+	}
+
+	public List<String> get_brandName_Price_Description(int index) {
+		logger.debug("Inside get_brandName_Price_Description function");
+		
+		
+		String brandName = getText(brandNameLocator,index).split("[0-9]")[0];
+		String description =getText(brandNameLocator,index) .split(brandName)[1];
+		String price = getText(priceLocator,index).replaceAll(" .*", "").substring(1);
+		
+		logger.debug("brandName---"+brandName);
+		logger.debug("description---"+description);
+		logger.debug("price---"+price);
+		
+		List<String> itemValues = new ArrayList<String>();
+		itemValues.add(brandName);
+		itemValues.add(description);
+		itemValues.add(price);
+
+		return itemValues;
+	}
+
+}
